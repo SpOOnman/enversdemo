e47077a8728a8e1b95dc5db3f8d7fbf82246a2c4
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2012-07-31 15:33:29 +0200

    Make sure newRevision doesn't throw NPE.

--- src/groovy/pl/refaktor/enversdemo/SpringSecurityRevisionListener.groovy
+++ src/groovy/pl/refaktor/enversdemo/SpringSecurityRevisionListener.groovy
@@ -1,13 +1,12 @@
 package pl.refaktor.enversdemo
 
 import org.hibernate.envers.RevisionListener
-import pl.refaktor.enversdemo.SpringSecurityServiceHolder
 
 class SpringSecurityRevisionListener implements RevisionListener {
 
     public void newRevision(Object entity) {
         UserRevisionEntity revisionEntity = (UserRevisionEntity) entity
-        User user = SpringSecurityServiceHolder.springSecurityService.currentUser
+        User user = SpringSecurityServiceHolder.springSecurityService?.currentUser
         revisionEntity.currentUser = user
     }
 }

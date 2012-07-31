7747fb8e2f9262b87d6a952ea36d850507a3567f
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2012-07-31 15:33:59 +0200

    Inject SpringSecurityService into holder.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,6 @@
+import pl.refaktor.enversdemo.SpringSecurityServiceHolder
+
 // Place your Spring DSL code here
 beans = {
+    springSecurityServiceHolder(SpringSecurityServiceHolder, ref('springSecurityService'))
 }

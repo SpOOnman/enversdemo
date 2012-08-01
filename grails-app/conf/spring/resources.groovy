d2c5f4ed6a58bd261a580559da5568d0eb99976d
Tomasz Kalkosiński <tomasz.kalkosinski@gmail.com>
2012-08-01 13:33:10 +0200

    Add configuration properties.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,4 +3,7 @@ import pl.refaktor.enversdemo.SpringSecurityServiceHolder
 // Place your Spring DSL code here
 beans = {
     springSecurityServiceHolder(SpringSecurityServiceHolder, ref('springSecurityService'))
+
+    System.setProperty('org.hibernate.envers.audit_table_prefix', 'AUD_')
+    System.setProperty('org.hibernate.envers.audit_table_suffix', '')
 }
